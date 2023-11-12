4aa8a88fab97996707131161c222af9f62f67b12
Elizabeth-05 <ramirezelizabeth05~@~gmail.com>
2023-11-12 23:47:20 +0100

    Añadir ejercicio01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,19 @@
 // Escribe tu código aquí:
+
+
+function imprimirHolaMundoAsyncrono(callbacks) {
+    console.log('Imprimiendo Hola Mundo....')
+setTimeout(() => {
+
+callbacks('Hola mundo')
+
+}, 2000)
+
+}
+
+imprimirHolaMundoAsyncrono((mensaje) => {
+    console.log(mensaje)
+
+})
+
+
